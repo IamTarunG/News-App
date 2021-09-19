0684df0caf0e9bfea629131d7f950daf677dfaf3
IamTarunG <tarungudipalli5@gmail.com>
2021-09-19 19:10:12 +0530

    Added Spinner Component

---
+++ src/Components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Spinner() {
+  return (
+    <div>
+      <div className="spinner-border text-primary" role="status">
+        <span className="sr-only"></span>
+      </div>
+    </div>
+  );
+}
